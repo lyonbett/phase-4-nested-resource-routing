a8120387846dc76b66510e5b24ebaeec6bc8a0d9
lyonbett <lyon.bett@student.moringaschool.com>
2023-07-10 04:25:16 +0300

    completed assignment

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,9 +1,26 @@
+
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def index
+    dog_house = DogHouse.all
+    render json: dog_house
+  end
 
   def show
     dog_house = DogHouse.find(params[:id])
     render json: dog_house
+  end
+
+  def reviews_index
+    dog_house = DogHouse.find(paramas[:dog_house_id])
+    reviews = DogHouse.reviews
+    render json: reviews, include: :dog_house
+  end
+
+  def review
+    review = Review.find(params[:id])
+    render json: review, include: :dog_house
   end
 
   private
